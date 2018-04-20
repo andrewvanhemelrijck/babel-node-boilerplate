f397bba375b266863308124999cff107ffccadfe
pigeontoe <electriclady1@gmail.com>
2018-04-20 00:23:34 -0400

    production tested and working

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _log = require('./log');
+
+var _log2 = _interopRequireDefault(_log);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _log2.default)('log.js working!');
+
+console.log('index.js');
+//# sourceMappingURL=index.js.map
---
+++ dist/log.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var log = function log(msg) {
+  console.log(msg);
+};
+
+exports.default = log;
+//# sourceMappingURL=log.js.map
